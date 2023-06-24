9f8cda973c797351786d6f86edd5fd0bb9419179
Fernando Mós <fernandomosp@gmail.com>
2023-06-24 17:52:09 -0300

    Chore(env): :heavy_plus_sign: Add .env and database connection string

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
+require('dotenv').config();
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ShopProductModule } from './shop-product/shop-product.module';
+import { ConfigModule } from '@nestjs/config';
+
+const { MONGO_DATABASE, MONGO_USERNAME, MONGO_PASSWORD } = process.env;
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
+    MongooseModule.forRoot(`mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@easybuyercluster.kk3hhta.mongodb.net/${MONGO_DATABASE}?retryWrites=true&w=majority`),
+    ShopProductModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
