38c431fa3d2e292955ac7a72cb5c6e53c15f0695
Fernando Mós <fernandomosp@gmail.com>
2023-06-24 15:33:24 -0300

    Chore(Swagger): :heavy_plus_sign: Added swagger to used in api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('EasyCore')
+    .setDescription('CRUD to used in todo list.')
+    .setVersion('1.0')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
+
+import { AppModule } from './app.module';
+
 bootstrap();
